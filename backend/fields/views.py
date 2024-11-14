f519449ba413c5594254a90f07e55e7eddf8ef3b
kosovact14 <tamarakosovac14@gmail.com>
2024-11-14 17:35:12 +0100

    Dodani tereni u bazu(sa ispravkom)

--- backend/fields/views.py
+++ backend/fields/views.py
@@ -4,15 +4,6 @@ from .models import Field
 from .serializers import FieldSerializer
 from rest_framework.views import APIView # type: ignore
 
-"""""
-class FieldSearchView(APIView):
-    def get(self, request, *args, **kwargs):
-        location = request.query_params.get('lokacija')
-        fields = Field.objects.filter(location__icontains=location)
-        serializer = FieldSerializer(fields, many=True)
-        return Response(serializer.data)
-"""""
-
 @api_view(['GET'])
 def getData(request):
     fields = Field.objects.all()
