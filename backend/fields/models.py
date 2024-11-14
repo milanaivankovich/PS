2f206bfaba0da7c68854b6332a991aa5b6342445
kosovact14 <tamarakosovac14@gmail.com>
2024-11-14 17:27:31 +0100

    Dodani tereni u bazu (sa ispravkom)

--- backend/fields/models.py
+++ backend/fields/models.py
@@ -5,7 +5,7 @@ class Field(models.Model):
     id = models.AutoField(primary_key=True)
     location = models.CharField(max_length=255)
     type_of_sport = models.CharField(max_length=255)
-    is_suspended = models.DateField(null=True, blank=True)
+    is_suspended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.location
--- backend/fields/urls.py
+++ backend/fields/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path # type: ignore
-from .views import FieldSearchView
+from . import views
 
 urlpatterns = [
-    path('teren/search/', FieldSearchView.as_view(), name='teren-search'),
+    path('fields/', views.getData),
 ]
