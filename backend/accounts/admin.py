1d0e6b5d759408eb263f9bb265068b04f997fc3b
kosovact14 <tamarakosovac14@gmail.com>
2024-11-16 12:22:37 +0100

    sa ispravkom za terene

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import BusinessSubject, StandardUser, Client
 from activities.models import Aktivnost #dodala Milica
+from fields.models import Field
 
 admin.site.register(BusinessSubject)
 admin.site.register(StandardUser)
 admin.site.register(Client)
+admin.site.register(Field)
 
 
