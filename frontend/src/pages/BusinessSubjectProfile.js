e029a0151062fcfc421ddf5428b19884302b00c8
sunci0089 <unav660@gmail.com>
2024-12-17 17:29:52 +0100

    Provjera tipa korisnika

--- frontend/src/pages/BusinessSubjectProfile.js
+++ frontend/src/pages/BusinessSubjectProfile.js
@@ -13,7 +13,7 @@ import CreatorImg from "../images/user.svg";
 import NewAdvertisementCard from "../components/NewAdvertisementCard.js";
 import { CiSettings } from "react-icons/ci";
 import { IoIosCloseCircle } from "react-icons/io";
-{/*proba za sponzorisane dogadjaje */}
+{/*proba za sponzorisane dogadjaje */ }
 const BusinessSubjectProfile = () => {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -27,7 +27,7 @@ const BusinessSubjectProfile = () => {
   const [activityHistory, setActivityHistory] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const [id, setID]=useState({
+  const [id, setID] = useState({
     "id": -1,
     "type": ''
   });
@@ -39,29 +39,29 @@ const BusinessSubjectProfile = () => {
   });
 
   useEffect(() => {
-      const fetchIDType = async ()=> {
-        await axios.get('http://localhost:8000/api/get-user-type-and-id/', {
-          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    const fetchIDType = async () => {
+      await axios.get('http://localhost:8000/api/get-user-type-and-id/', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      })
+        .then((request) => {
+          if (request.data.type === 'Client')
+            window.location.replace("/userprofile");
+          setID(request.data);
         })
-          .then((request) => {
-            if(request.data.type==='Client')
-              window.location.replace("/userprofile");
-            setID(request.data);
-          })
-          .catch((error) => {
-            console.error("Error getting ID: ",error);
-            alert("Neuspjesna autorizacija. Molimo ulogujte se ponovo... ");
-          {/*window.location.replace("/login");*/}
-          });
-        };
+        .catch((error) => {
+          console.error("Error getting ID: ", error);
+          alert("Neuspjesna autorizacija. Molimo ulogujte se ponovo... ");
+          {/*window.location.replace("/login");*/ }
+        });
+    };
 
-      fetchIDType();
-    },[]);
-  
-    useEffect(() => {
+    fetchIDType();
+  }, []);
 
-      const fetchUserData = async ()=> {
-      await axios.get('http://localhost:8000/api/business-subject/'+id.id+'/')
+  useEffect(() => {
+
+    const fetchUserData = async () => {
+      await axios.get('http://localhost:8000/api/business-subject/' + id.id + '/')
         .then(response => {
           setSubjectData(response.data);
         })
@@ -69,154 +69,154 @@ const BusinessSubjectProfile = () => {
           console.error('Error fetching data: ', error);
           alert('Error fetching data');
         });
-      };
+    };
 
-      if(id.id!==-1)
-        fetchUserData();
-    }, [id]);
+    if (id.id !== -1)
+      fetchUserData();
+  }, [id]);
 
-    const [eventsData, setEventsData] = useState([]);
-    const [selectionTitle, setSelectionTitle] = useState('Događaji');
-    const [selectionSubtitle,setSelectionSubtitle] = useState('Događaji koje je kreirao korisnik');
-    //napraviti chain sa fetch
+  const [eventsData, setEventsData] = useState([]);
+  const [selectionTitle, setSelectionTitle] = useState('Događaji');
+  const [selectionSubtitle, setSelectionSubtitle] = useState('Događaji koje je kreirao korisnik');
+
   // Funkcija za dohvaćanje podataka za različite kartice
   useEffect(() => {
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      switch (activeTab) {
-        case "events":  //todo
-          const eventsResponse = await axios.get('http://localhost:8000/api/advertisements/businesssubject/'+id.id+'/');
-          setEventsData(eventsResponse.data);
-          break;
-        case "favorites":
-          const favoritesResponse = await axios.get('http://localhost:8000/api/user/favorites', {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-          });
-          setFavorites(favoritesResponse.data);
-          break;
-        case "messages":
-          const messagesResponse = await axios.get('http://localhost:8000/api/user/messages', {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-          });
-          setMessages(messagesResponse.data);
-          break;
-        case "activity":
-          const activityResponse = await axios.get('http://localhost:5000/api/user/activity', {
-            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-          });
-          setActivityHistory(activityResponse.data);
-          break;
-        default:
-          break;
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        switch (activeTab) {
+          case "events":
+            const eventsResponse = await axios.get('http://localhost:8000/api/advertisements/businesssubject/' + id.id + '/');
+            setEventsData(eventsResponse.data);
+            break;
+          case "favorites":
+            const favoritesResponse = await axios.get('http://localhost:8000/api/user/favorites', {
+              headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+            });
+            setFavorites(favoritesResponse.data);
+            break;
+          case "messages":
+            const messagesResponse = await axios.get('http://localhost:8000/api/user/messages', {
+              headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+            });
+            setMessages(messagesResponse.data);
+            break;
+          case "activity":
+            const activityResponse = await axios.get('http://localhost:5000/api/user/activity', {
+              headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+            });
+            setActivityHistory(activityResponse.data);
+            break;
+          default:
+            break;
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    };
 
-  
-    fetchData();
+    if (id.id !== -1)
+      fetchData();
   }, [id, activeTab]);
 
   return (
     <body>
       <header className="userprofile-menu">
-        <MenuBar variant={[id.id!==-1 ? "registered" : "unregistered"]} search={true} />
+        <MenuBar variant={[id.id !== -1 ? "registered" : "unregistered"]} search={true} />
       </header>
       <div className="userprofile-body">
         <div className="userprofile-header">
-          <img src={subjectData?.profile_picture!==null ? subjectData.profile_picture : CreatorImg} 
-          className="userprofilepreview-image" alt="Creator" />
+          <img src={subjectData?.profile_picture !== null ? subjectData.profile_picture : CreatorImg}
+            className="userprofilepreview-image" alt="Creator" />
           <div className='name-surname-username'>
-            <h0 className="userprofile-name">{subjectData.nameSportOrganization }</h0>
+            <h0 className="userprofile-name">{subjectData.nameSportOrganization}</h0>
             <h1 className="userprofile-subtitle">{subjectData.description}</h1>
           </div>
           <CiSettings className='edituserprofile-button' onClick={() => window.location.replace('/editbusinessprofile')} />
         </div>
-          <div>
-            <nav className="profile-tabs">
-          <button className={`tab-button ${activeTab === "events" ? "active" : ""}`} 
-          onClick={() => {
-            setSelectionTitle('Događaji'); 
-            setSelectionSubtitle('Događaji koje je kreirao korisnik');
-            setActiveTab("events");
-          }}>Događaji</button>
-          <button className={`tab-button ${activeTab === "favorites" ? "active" : ""}`} onClick={() => 
-            {
+        <div>
+          <nav className="profile-tabs">
+            <button className={`tab-button ${activeTab === "events" ? "active" : ""}`}
+              onClick={() => {
+                setSelectionTitle('Događaji');
+                setSelectionSubtitle('Događaji koje je kreirao korisnik');
+                setActiveTab("events");
+              }}>Događaji</button>
+            <button className={`tab-button ${activeTab === "favorites" ? "active" : ""}`} onClick={() => {
               setSelectionTitle('Omiljeno'); setSelectionSubtitle('Vaši omiljeni događaji');
-              setActiveTab("favorites")}}>Omiljeno</button>
-          <button className={`tab-button ${activeTab === "messages" ? "active" : ""}`} onClick={() =>
-            {
+              setActiveTab("favorites")
+            }}>Omiljeno</button>
+            <button className={`tab-button ${activeTab === "messages" ? "active" : ""}`} onClick={() => {
               setSelectionTitle('Poruke'); setSelectionSubtitle('');
-              setActiveTab("messages")}  
-            
-          }>Poruke</button>
-          <button className={`tab-button ${activeTab === "activity" ? "active" : ""}`} onClick={() => 
-            {
+              setActiveTab("messages")
+            }
+
+            }>Poruke</button>
+            <button className={`tab-button ${activeTab === "activity" ? "active" : ""}`} onClick={() => {
               setSelectionTitle('Istoriјa aktivnosti'); setSelectionSubtitle('Događaji kojima se korisnik pridružio');
-              setActiveTab("activity")}}>Istorija Aktivnosti</button>
-        </nav></div>
-        
-          <div className="userprofile-selection">
-            <h1 className="userprofile-name">{selectionTitle}</h1>
-            <h2 className="userprofile-subtitle">{selectionSubtitle}</h2>
-            <section className="tab-content">
-              {loading ? (
-                <p>Učitavanje...</p>
-                ) : (
+              setActiveTab("activity")
+            }}>Istorija Aktivnosti</button>
+          </nav></div>
+
+        <div className="userprofile-selection">
+          <h1 className="userprofile-name">{selectionTitle}</h1>
+          <h2 className="userprofile-subtitle">{selectionSubtitle}</h2>
+          <section className="tab-content">
+            {loading ? (
+              <p>Učitavanje...</p>
+            ) : (
               <div>
                 {activeTab === "events" && (
                   <div className="events-section">
+                    <div className="scroll-bar-user-profile">
+                      {Array.isArray(eventsData) && eventsData.map((activity) => (
+                        <SponsoredEventCard key={activity.id} event={activity} />
+                      ))}
+                    </div>
+                    {(id.id !== -1) ? (
+                      <button className="create-event-button" onClick={() => toggleFloatingWindow()}>
+                        + Novi događaj
+                      </button>
+                    ) : null}
+                    {isVisible ? (
+                      <div>
+                        <NewAdvertisementCard user={subjectData} pk={id.id} className="new-event-card" />
+                        <IoIosCloseCircle className="close-icon-new-advertisement" onClick={() => toggleFloatingWindow()} />
+                      </div>
+                    ) : null}
+                  </div>
+                )}
+
+                {activeTab === "favorites" && (
                   <div className="scroll-bar-user-profile">
-                  {Array.isArray(eventsData) && eventsData.map((activity) => (
-                    <SponsoredEventCard key={activity.id} event={activity} />
-                  ))}
-                </div>
-                { (id.id!==-1) ? (
-                  <button className="create-event-button" onClick={()=>toggleFloatingWindow()}>
-                    + Novi događaj
-                  </button> 
-                  ) : null }
-                { isVisible ? (
-                <div>
-                  <NewAdvertisementCard user={subjectData} pk={id.id} className="new-event-card" />
-                  <IoIosCloseCircle className="close-icon-new-advertisement" onClick={()=>toggleFloatingWindow()}/>
-                </div>
-                ): null }
+                    {favorites.map((favorite) => (
+                      <FavoriteCard key={favorite.id} title={favorite.title} description={favorite.description} />
+                    ))}
+                  </div>
+                )}
+
+                {activeTab === "messages" && (
+                  <div className="messages-cards-container">
+                    {messages.map((message) => (
+                      <MessageCard key={message.id} sender={message.sender} content={message.content} />
+                    ))}
+                  </div>
+                )}
+
+                {activeTab === "activity" && (
+                  <div className="scroll-bar-user-profile">
+                    {activityHistory.map((activity) => (
+                      <ActivityCard key={activity.id} activity={activity} />
+                    ))}
+                  </div>
+                )}
               </div>
             )}
-
-            {activeTab === "favorites" && (
-                <div className="scroll-bar-user-profile">
-                  {favorites.map((favorite) => (
-                    <FavoriteCard key={favorite.id} title={favorite.title} description={favorite.description} />
-                  ))}
-                </div>
-            )}
-
-            {activeTab === "messages" && (
-                <div className="messages-cards-container">
-                  {messages.map((message) => (
-                    <MessageCard key={message.id} sender={message.sender} content={message.content} />
-                  ))}
-                </div>
-            )}
-
-            {activeTab === "activity" && (
-                <div className="scroll-bar-user-profile">
-                  {activityHistory.map((activity) => (
-                    <ActivityCard key={activity.id} activity={activity} />
-                  ))}
-                </div>
-            )}
-          </div>
-        )}
-              </section>
-          </div>
+          </section>
         </div>
+      </div>
       <Footer />
     </body>
   );
--- frontend/src/pages/EditUserProfile.js
+++ frontend/src/pages/EditUserProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./EditUserProfile.css";
 import profileImage from "../images/user.svg";
 import MenuBar from "../components/MenuBar.js";
@@ -6,45 +6,48 @@ import Footer from "../components/Footer.js";
 import axios from "axios";
 import AddPhoto from '../components/AddPhoto.js';
 import { IoIosCloseCircle } from "react-icons/io";
-import {Modal, Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 const EditUserProfile = () => {
-    const [id, setID]=useState({
-      "pk": -1,
-    });
-    
-    const [userData, setUserData] = useState({
-      "first_name": '',
-      "last_name": '',
-      "username": '',
-      "email": '',
-      "profile_picture": null
-    });
+  const [id, setID] = useState({
+    "id": -1,
+    "type": '',
+  });
 
-    useEffect(() => {
+  const [userData, setUserData] = useState({
+    "first_name": '',
+    "last_name": '',
+    "username": '',
+    "email": '',
+    "profile_picture": null
+  });
 
-      const fetchID = async ()=> {
-      await axios.get('http://localhost:8000/api/get-client-id/', {
+  useEffect(() => {
+
+    const fetchID = async () => {
+      await axios.get('http://localhost:8000/api/get-user-type-and-id/', {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
       })
         .then((request) => {
+          if (request.data.type === 'BusinessSubject')
+            window.location.replace("/editbusinesssubject");
           setID(request.data);
         })
         .catch((error) => {
-          console.error("Error getting ID: ",error);
+          console.error("Error getting ID: ", error);
           alert("Neuspjesna autorizacija. Molimo ulogujte se ponovo... ");
           window.location.replace("/login");
         });
-      };
+    };
 
-      fetchID();
-    },[]);
-  
-    useEffect(() => {
+    fetchID();
+  }, []);
 
-      const fetchUserData = async ()=> {
-      await axios.get('http://localhost:8000/api/client/'+id.pk+'/')
+  useEffect(() => {
+
+    const fetchUserData = async () => {
+      await axios.get('http://localhost:8000/api/client/' + id.id + '/')
         .then(response => {
           setUserData(response.data);
         })
@@ -52,16 +55,16 @@ const EditUserProfile = () => {
           console.error('Error fetching data: ', error);
           alert('Error fetching data');
         });
-      };
+    };
 
-      if(id.pk!==-1)
-        fetchUserData();
-    }, [id]);
-    
+    if (id.id !== -1)
+      fetchUserData();
+  }, [id]);
+
 
   const handleUpdate = async (e) => {
     e.preventDefault();
-    await axios.put('http://localhost:8000/api/client/'+id.pk+'/edit/', userData, {
+    await axios.put('http://localhost:8000/api/client/' + id.id + '/edit/', userData, {
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
     })
       .then((response) => {
@@ -78,40 +81,43 @@ const EditUserProfile = () => {
     'new_password': '',
     'confirm_password': '',
   });
-  
 
-  const handlePasswordUpdate =async (e, password) => {
+
+  const handlePasswordUpdate = async (e, password) => {
     e.preventDefault();
-    if (password.new_password===password.old_password){
+    if (password.new_password === password.old_password) {
       alert("Nova lozinka se podudara sa starom. Odaberite drugu lozinku!");
     }
-    else if (password.new_password!==password.confirm_password){
-      alert("Nova lozinka se ne podudara sa potvrdom");}
+    else if (password.new_password !== password.confirm_password) {
+      alert("Nova lozinka se ne podudara sa potvrdom");
+    }
     else {
-      await axios.put('http://localhost:8000/api/client/'+id.pk+'/edit/', password, {
+      await axios.put('http://localhost:8000/api/client/' + id.id + '/edit/', password, {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
       })
-      .then((response) => {
-        console.log("Password updated successfully:", response.data);
-        alert("Lozinka je azurirana"+response.data+"");
-      })
-      .catch((error) => {
-        console.error("There was an error updating the data:", error);
-        alert("Doslo je do greške prilikom ažuriranja...");
-      });
-    }};
+        .then((response) => {
+          console.log("Password updated successfully:", response.data);
+          alert("Lozinka je azurirana" + response.data + "");
+        })
+        .catch((error) => {
+          console.error("There was an error updating the data:", error);
+          alert("Doslo je do greške prilikom ažuriranja...");
+        });
+    }
+  };
 
   const handleDeactivation = async () => {
     //dodati neki message popup da se potvrdi deaktivacija i onda pozvati on button click
     //sta sa poslovnim subjektom
     await axios.post('http://localhost:8000/api/deactivate-client/', userData.username)
-    .then((response) => {
-      console.log("Password updated successfully:", response.data);
-      alert("Nalog je deaktiviran"+response.data);
-    })
-    .catch((error)=>{
-      console.error("There was an error updating the data:", error);
-      alert("Doslo je do greške prilikom deaktivacije...");});
+      .then((response) => {
+        console.log("Password updated successfully:", response.data);
+        alert("Nalog je deaktiviran" + response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error updating the data:", error);
+        alert("Doslo je do greške prilikom deaktivacije...");
+      });
   };
 
   const handleLogout = () => {
@@ -138,17 +144,17 @@ const EditUserProfile = () => {
       <div className="EditUserProfileBody">
         <div className="EditUserProfileDialog">
           <img
-            src={userData.profile_picture!==null ? userData.profile_picture: profileImage}
+            src={userData.profile_picture !== null ? userData.profile_picture : profileImage}
             alt="Circular Image"
             className="EditProfileImage"
           />
-          <button className="NewImage" onClick={()=>toggleDialog()} >
+          <button className="NewImage" onClick={() => toggleDialog()} >
             Nova slika
           </button>
-          {isDialogOpen && id.pk!==-1 ?  <div>
-                  <AddPhoto className="new-event-card" userId={id} />
-                  <IoIosCloseCircle className="close-icon-orange" onClick={()=>toggleDialog()}/>
-                </div>  : null}
+          {isDialogOpen && id.id !== -1 ? <div>
+            <AddPhoto className="new-event-card" userId={id} />
+            <IoIosCloseCircle className="close-icon-orange" onClick={() => toggleDialog()} />
+          </div> : null}
           <form onSubmit={handleUpdate}>
             <label className="EditProfileLabel">Ime</label>
             <input
@@ -156,15 +162,15 @@ const EditUserProfile = () => {
               value={userData.first_name}
               className="EditProfileInput"
               required
-              onChange={(e) => setUserData(prevData=>({...prevData, first_name: e.target.value }))}
+              onChange={(e) => setUserData(prevData => ({ ...prevData, first_name: e.target.value }))}
             />
-             <label className="EditProfileLabel">Prezime</label>
+            <label className="EditProfileLabel">Prezime</label>
             <input
               type="name"
               value={userData.last_name}
               className="EditProfileInput"
               required
-              onChange={(e) => setUserData(prevData=>({...prevData, last_name: e.target.value }))}
+              onChange={(e) => setUserData(prevData => ({ ...prevData, last_name: e.target.value }))}
             />
             <label className="EditProfileLabel">Korisničko ime</label>
             <input
@@ -172,7 +178,7 @@ const EditUserProfile = () => {
               value={userData.username}
               className="EditProfileInput"
               required
-              onChange={(e) => setUserData(prevData=>({...prevData, username: e.target.value }))}
+              onChange={(e) => setUserData(prevData => ({ ...prevData, username: e.target.value }))}
             />
             <label className="EditProfileLabel">E-mail</label>
             <input
@@ -180,7 +186,7 @@ const EditUserProfile = () => {
               value={userData.email}
               className="EditProfileInput"
               required
-              onChange={(e) => setUserData(prevData=>({...prevData, email: e.target.value }))}
+              onChange={(e) => setUserData(prevData => ({ ...prevData, email: e.target.value }))}
             />
             <button
               className="EditProfileButton"
@@ -190,65 +196,65 @@ const EditUserProfile = () => {
               Sačuvaj
             </button>
           </form>
-          <form onSubmit={(e)=>handlePasswordUpdate(e,password)}>
-          <label className="EditProfileLabel">Stara lozinka</label>
-          <input
-            type="password"
-            placeholder="Unesi staru lozinku"
-            className="EditProfileInput"
-            onChange={(e) => setPassword(prevData=>({...prevData, old_password: e.target.value }))}
-            required
-          />
-          <label className="EditProfileLabel">Nova lozinka</label>
-          <input
-            type="password"
-            placeholder="Unesi novu lozinku"
-            className="EditProfileInput"
-            onChange={(e) => setPassword(prevData=>({...prevData, new_password: e.target.value }))}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Potvrdi novu lozinku"
-            className="EditProfileInput"
-            onChange={(e) => setPassword(prevData=>({...prevData, confirm_password: e.target.value }))}
-            required
-          />
-          <button 
-            className="EditProfileButton" 
-            id="PromijeniLozinkuButton"
-            type="submit">
-            Promijeni lozinku
-          </button>
+          <form onSubmit={(e) => handlePasswordUpdate(e, password)}>
+            <label className="EditProfileLabel">Stara lozinka</label>
+            <input
+              type="password"
+              placeholder="Unesi staru lozinku"
+              className="EditProfileInput"
+              onChange={(e) => setPassword(prevData => ({ ...prevData, old_password: e.target.value }))}
+              required
+            />
+            <label className="EditProfileLabel">Nova lozinka</label>
+            <input
+              type="password"
+              placeholder="Unesi novu lozinku"
+              className="EditProfileInput"
+              onChange={(e) => setPassword(prevData => ({ ...prevData, new_password: e.target.value }))}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Potvrdi novu lozinku"
+              className="EditProfileInput"
+              onChange={(e) => setPassword(prevData => ({ ...prevData, confirm_password: e.target.value }))}
+              required
+            />
+            <button
+              className="EditProfileButton"
+              id="PromijeniLozinkuButton"
+              type="submit">
+              Promijeni lozinku
+            </button>
           </form>
           <button className="EditProfileButton"
-          onClick={()=>handleLogout()}>Odjavi se</button>
-          <button className="EditProfileButton" onClick={()=>toggleDeactivation()}>Deaktiviraj nalog</button>
-          { isDeactivating ? <div
-           className="dimmer"
-           >
-          <Modal.Dialog className='modal-bootstrap' >
-            <Modal.Header>
-              <Modal.Title className='Naslov'>DEAKTIVACIJA NALOGA</Modal.Title>
-            </Modal.Header>
+            onClick={() => handleLogout()}>Odjavi se</button>
+          <button className="EditProfileButton" onClick={() => toggleDeactivation()}>Deaktiviraj nalog</button>
+          {isDeactivating ? <div
+            className="dimmer"
+          >
+            <Modal.Dialog className='modal-bootstrap' >
+              <Modal.Header>
+                <Modal.Title className='Naslov'>DEAKTIVACIJA NALOGA</Modal.Title>
+              </Modal.Header>
 
-            <Modal.Body>
-          <p className='modal-body'>Da li sigurno želite deaktivirati vaš nalog?</p>
-            </Modal.Body>
+              <Modal.Body>
+                <p className='modal-body'>Da li sigurno želite deaktivirati vaš nalog?</p>
+              </Modal.Body>
 
-          <Modal.Footer>
-            <Button 
-            className="EditProfileButton" 
-            id='OdustaniButton'
-            onClick={()=>toggleDeactivation()} 
-            variant="primary">Odustani</Button>
-            <Button 
-            className="EditProfileButton" 
-            onClick={()=>handleDeactivation()}
-            variant="secondary">Nastavi</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-        </div> : null }
+              <Modal.Footer>
+                <Button
+                  className="EditProfileButton"
+                  id='OdustaniButton'
+                  onClick={() => toggleDeactivation()}
+                  variant="primary">Odustani</Button>
+                <Button
+                  className="EditProfileButton"
+                  onClick={() => handleDeactivation()}
+                  variant="secondary">Nastavi</Button>
+              </Modal.Footer>
+            </Modal.Dialog>
+          </div> : null}
         </div>
       </div>
       <Footer />
