3999a4cfa8036a27fe0363a2c5e24c5b89d9fd85
anastasijamilenic <milenicanastasija@gmail.com>
2025-01-05 19:57:35 +0100

    Fix registracija 2

--- frontend/src/pages/RegisterRekreativac.js
+++ frontend/src/pages/RegisterRekreativac.js
@@ -55,7 +55,7 @@ function RegisterRekreativac() {
     password: "",
     confirmPassword: "",
     email: "",
-    image:null,
+    image:""
   });
 
   const [passwordCriteria, setPasswordCriteria] = useState({
