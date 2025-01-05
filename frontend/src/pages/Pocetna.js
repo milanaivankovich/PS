29901d8390cbf6abec2f82dd3fd2f950a13a7f94
sunci0089 <unav660@gmail.com>
2025-01-05 12:20:50 +0100

    Prikaz menija na pocetnoj strani

--- frontend/src/pages/Pocetna.js
+++ frontend/src/pages/Pocetna.js
@@ -1,15 +1,51 @@
 import "./Pocetna.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MenuBar from "../components/MenuBar.js";
 import Search from "../components/Search.js";
 import Footer from "../components/Footer.js";
 import SearchFields from "../components/SearchFields.js";
+import axios from "axios";
 
 function Pocetna() {
+
+  const [id, setID] = useState({
+    "id": -1,
+    "type": ''
+  });
+
+  useEffect(() => {
+    const fetchIDType = async () => {
+      await axios.get('http://localhost:8000/api/get-user-type-and-id/', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      })
+        .then((request) => {
+          if (request.data.type === 'BusinessSubject')
+            window.location.replace("/userprofile1");
+          setID(request.data);
+        })
+        .catch((error) => {
+          console.error("Error getting ID: ", error);
+          alert("Neuspjesna autorizacija. Molimo ulogujte se ponovo... ");
+          {/*window.location.replace("/login");*/ }
+        });
+    };
+    const fetchID = async () => {
+      await axios.get('http://localhost:8000/api/get-client-id/', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      })
+        .then((request) => {
+          setID(request.data);
+        })
+        .catch((error) => {
+          console.error("Error getting ID: ", error);
+        });
+    };
+    fetchIDType();
+  }, []);
   return (
     <div className="pocetna">
       <header className="pocetna-header">
-        <MenuBar variant={["unregistered"]} search={false} />
+        <MenuBar variant={[id.id !== -1 ? "registered" : "unregistered"]} search={true} />
       </header>
       <div className="pocetna-body">
         <div className="search">
